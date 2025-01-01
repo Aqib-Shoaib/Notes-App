10cee09c174afbd737896f8b0b3f25f0006a0e1e
Aqib-Shoaib <bluepitviper1233@gmail.com>
2025-01-01 15:51:23 +0500

    create page partially done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette:{
+    primary:{
+      main: '#fefefe',
+    },
+    secondary: purple,
+  },
+  typography:{
+    fontFamily:'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightmedium: 600,
+    fontWeightbold: 700,
+
+  }
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Notes />} />
         <Route path="/create" element={<Create />} />
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,54 @@
-import { Button, Container, Typography } from "@mui/material";
-import React from "react";
+import { Button, Container, Typography, TextField } from "@mui/material";
+import React, { useState } from "react";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
+//the tutor taught how to style the components as you wish using makeStyled that is deprecated, but this down here
+//is the new way, you can also use "sx" prop, google it or ask CHATGPT , it will guide you, moreover,
+//this way of styling also resembles that of "styled-components" library that is another great npm package
+
+// const CustomBtn = styled(Button)({
+//   fontSize: 60,
+//      backgroundColor: "violet",
+//      "&:hover": {
+//        background: "blue",
+//      },
+// });
+
+// const CustomTitle = styled(Typography)({
+//   title: {
+//     textDecoration: "underline",
+//     marginBottom: 20,
+//   },
+// });
+
+const textFieldStyles = {
+  marginTop: "20px",
+  marginBottom: "20px",
+  display: "block",
+};
+
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setdetailsError] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setdetailsError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (details === "") {
+      setdetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -14,16 +60,40 @@ export default function Create() {
         Create a new note
       </Typography>
 
-      <Button
-        type="submit"
-        color="secondary"
-        variant="contained"
-        onClick={() => console.log("s")}
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
-      <br />
+      <form noValidate onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          className="textField"
+          sx={textFieldStyles}
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          multiline
+          rows={4}
+          error={detailsError}
+          className="textField"
+          sx={textFieldStyles}
+        />
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
